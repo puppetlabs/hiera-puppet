09092a0f6b2a46c7eaab33a476e577036d290825
Hunter Haugen <h.haugen@gmail.com>
2012-01-23 14:51:06 +0000

    Updating tests with more testy-like mentality
    So.. we don't care what lookup returns since that is not an aspect of
    the Hiera functions.
    
    We expect hiera libraries to be present during the test, since unit
    tests are still tested in their respective gemsets in Jenkins

--- spec/unit/parser/functions/hiera_hash_spec.rb
+++ spec/unit/parser/functions/hiera_hash_spec.rb
@@ -6,42 +6,37 @@ describe 'Puppet::Parser::Functions' do
   describe "#hiera_hash" do
     before :each do
       @scope = Puppet::Parser::Scope.new
+      configfile = File.join([File.dirname(Puppet.settings[:config]), "hiera.yaml"])
+      Hiera.any_instance.stubs(:lookup).returns({}) # We don't test what Hiera returns
+      File.stubs(:exist?).with(configfile).returns(true)
+      File.stubs(:exist?).with("/var/lib/hiera/common.yaml").returns(true)
+      YAML.stubs("load_file").returns( {:backends => ["yaml"], :hierarchy => "common", :logger => "console"})
     end
     it 'should exist' do
       Puppet::Parser::Functions.function(:hiera_hash).should == 'function_hiera_hash'
     end
-    describe 'accepting arguments' do
+    describe 'when accepting arguments it' do
       before :each do
-        File.expects(:exist?).with(File.join([File.dirname(Puppet.settings[:config]), "hiera.yaml"])).returns(true)
-        File.expects(:exist?).with("/var/lib/hiera/common.yaml").returns(true)
-        YAML.stubs("load_file").returns({:backends => ["yaml"], :hierarchy => "common", :logger => "console"})
-        #Hiera::Config.stubs("load").returns({:backends => ["yaml"], :hierarchy => "common", :logger => "console"})
         Puppet::Parser::Functions.function(:hiera_hash)
       end
-      it 'should optionally take a default hash value' do
-        @scope.function_hiera_hash(['key', {'key' => 'default'}]).should == { 'key' => 'default' }
+      it 'should require a key argument' do
+        expect { @scope.function_hiera_hash([]) }.should raise_error(Puppet::ParseError)
       end
-      it 'should accept an empty hash default value'
-      it 'should deny a non-hash default value'
-      it 'should optionally take a default value and override level'
-      it 'should accept an empty override level'
+      it 'should optionally take a default hash value' do
+        @scope.function_hiera_hash(['key', {'key' => 'default'}])
+      end
+      it 'should accept an empty hash default value' do
+        @scope.function_hiera_hash(['key', {}])
+      end
+      it 'should deny a non-hash default value' do
+        expect { @scope.function_hiera_hash(['key', 'default']) }.should raise_error(Puppet::ParseError)
+      end
+      it 'should optionally take a default value and override level' do
+        @scope.function_hiera_hash(['key', {'key' => 'default'}, 'common'])
+      end
+      it 'should accept an empty override level for empty variables' do
+        @scope.function_hiera_hash(['key', {'key' => 'default'}, ''])
+      end
     end
-    describe 'returning answers' do
-      it 'should return result'
-      it 'should return default answer if no result'
-      it 'should return result instead of default'
-      it 'should return empty result'
-      it 'should return empty default'
-      it 'should not return nil result'
-      it 'should not return nil default'
-    end
-  end
-end
-
-describe 'Puppet::Parser::Functions#hiera_hash argument error' do
-  it 'should require a key argument' do
-    Puppet::Parser::Functions.function(:hiera_hash)
-    @scope = Puppet::Parser::Scope.new
-    expect { @scope.function_hiera_hash([]) }.should raise_error(Puppet::ParseError)
   end
 end
